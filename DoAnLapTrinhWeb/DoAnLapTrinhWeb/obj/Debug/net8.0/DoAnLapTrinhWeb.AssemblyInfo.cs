276a10bd2942680733c8387865e5fa397664a072
Tran Viet Hung <hungtgdd68@gmail.com>
2024-04-20 23:22:53 +0700

    Hoan thanh chuc nang: Doc, Danh gia, Lich su

--- DoAnLapTrinhWeb/DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
+++ DoAnLapTrinhWeb/DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0ef8c85eb51e76ac4829354cc04ea1c71899d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cba2c87f736267c1a6a55367c02c687e7c52971")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
